0df5a98f9e7a673f95544bf1e7fdd09e2318264c
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2023-12-27 17:41:43 +0300

    Added solution for Sort the People

---
+++ progress_sheet/Week-1/sort-the-people.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        n = len(names)
+        for i in range(n):
+            max_index = i 
+            for j in range(i + 1, n):
+                if heights[j] > heights[max_index]:
+                    max_index = j
+            
+            heights[max_index],heights[i] = heights[i], heights[max_index]
+            names[max_index], names[i] = names[i], names[max_index]
+
+        return names
+        
