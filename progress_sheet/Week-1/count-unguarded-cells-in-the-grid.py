498811ff9e5f12e104a831085e0a56cc46582ac9
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2023-12-30 12:34:05 +0300

    Added solution for Count Unguarded Cells in the Grid

--- progress_sheet/Week-1/count-unguarded-cells-in-the-grid.py
+++ progress_sheet/Week-1/count-unguarded-cells-in-the-grid.py
@@ -1,18 +1,20 @@
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
         mat = [[0 for i in range(n)] for _ in range(m)]
-        guards = set([tuple(g) for g in guards])
-        walls = set([tuple(w) for w in walls])
-        t = 0
+        for r, c in walls:
+            mat[r][c] = 3
+        for r, c in guards:
+            mat[r][c] = 1
+        t = len(walls) + len(guards)
 
         def move(r,c):
             t = 0
             # move up
             ur = r - 1
             while ur >= 0:
-                if (ur,c) in walls or (ur,c) in guards:
+                if mat[ur][c] in (1,3):
                     break
-                if mat[ur][c] == 0:
+                elif mat[ur][c] == 0:
                     t += 1
                     mat[ur][c] = 2
                 ur -= 1
@@ -20,27 +22,27 @@ class Solution:
             # move down
             dr = r + 1
             while dr < m:
-                if (dr,c) in walls or (dr,c) in guards:
+                if mat[dr][c] in (1,3):
                     break
-                if mat[dr][c] == 0:
+                elif mat[dr][c] == 0:
                     t += 1
                     mat[dr][c] = 2
                 dr += 1
             # move right
             rc = c + 1
             while rc < n:
-                if (r,rc) in walls or (r,rc) in guards:
+                if mat[r][rc] in (1,3):
                     break
-                if mat[r][rc] == 0:
+                elif mat[r][rc] == 0:
                     t += 1
                     mat[r][rc] = 2
                 rc += 1
             # move left
             lc = c - 1
             while lc >= 0:
-                if (r,lc) in walls or (r,lc) in guards:
+                if mat[r][lc] in (1,3):
                     break
-                if mat[r][lc] == 0:
+                elif mat[r][lc] == 0:
                     t += 1
                     mat[r][lc] = 2
                 lc -= 1
@@ -49,4 +51,4 @@ class Solution:
         for r, c in guards:
             t += move(r,c)
     
-        return m * n - t - len(guards) - len(walls)
+        return m * n - t
