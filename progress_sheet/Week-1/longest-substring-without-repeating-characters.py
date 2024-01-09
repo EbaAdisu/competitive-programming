5ae24f532e4104ca54e5383dd5cd0946cad0a03c
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2024-01-09 17:35:27 +0300

    Added solution for Longest Substring Without Repeating Characters

---
+++ progress_sheet/Week-1/longest-substring-without-repeating-characters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        window =defaultdict(int)
+        l = 0
+        ans = 0
+
+        for r in range(len(s)):
+            if s[r] in window and window[s[r]] >= l :
+                l = window[s[r]]+1
+            window[s[r]] = r
+            ans = max(ans, r-l+1)
+        return ans
+
+
+
+        
