0b18e2de535ddc6b0f4368c55d0756da60292e69
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2024-01-17 08:30:04 +0300

    Added solution for Circular Array Loop

--- progress_sheet/01_sliding_window(-58)/circular-array-loop.py
+++ progress_sheet/01_sliding_window(-58)/circular-array-loop.py
@@ -1,26 +1,25 @@
 class Solution:
     def circularArrayLoop(self, nums: List[int]) -> bool:
-
-        for i in range(len(nums)):
-            if nums[i] < 0:
-                dire = -1
-            else:
-                dire = 1
-
-            t = 0
-            pos = i
-
-            while t <= len(nums):
-                if nums[pos] * dire < 0 or nums[pos] == 0 and dire == -1:
+        bad = set()
+        n = len(nums)
+        for i,e in enumerate(nums):
+            seen = set()
+            if e in bad:
+                continue
+            curr = i
+            pos = True if e >= 0 else False 
+            pre = None
+            while curr not in seen and curr not in bad:
+                seen.add(curr)
+                bad.add(curr)
+                pre = curr
+                curr = (e+curr)%n
+                e = nums[curr]
+                if pos and e<0 or not pos and e >=0:
+                    pre = None
                     break
-
-                pos += nums[pos]
-                pos %= len(nums)
-                if t == 0 and pos ==i:
-                    break
-                elif pos == i:
-                    return True
-
-                t += 1
-
+            if pre != None and pre != curr and len(seen) > 1:
+                return True
         return False
+
+        
