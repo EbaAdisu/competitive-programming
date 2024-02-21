49c8042a100ca743d00e2630b9b75880f5da49e1
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2024-02-21 17:24:20 +0300

    Add solution for Jump Game

---
+++ progress_sheet/02_prefix(70-)/leetcode/jump-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        curr = 0
+        for i, e in enumerate(nums):
+            curr = max(e,curr)
+            if curr == 0 and i != len(nums)-1:
+                return False
+            if i + curr >= len(nums)-1:
+                return True
+            curr -= 1
+        # return True
+        
