99499456c3673c6e21551acbcb5d622bf0ace9f7
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2024-05-11 17:26:58 +0300

    Created 'House Robber III' by A2SV Hub

---
+++ 11-May-2024/House Robber III 146829.py
@@ -0,0 +1,20 @@
+# Problem: House Robber III - https://leetcode.com/problems/house-robber-iii/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def trav(root):
+            if root:
+                left = trav(root.left)
+                right = trav(root.right)
+                child = left[1] + right[1] + root.val
+                grandchild = left[0] + right[0]
+                return max(child, grandchild), (grandchild)
+            return 0,0
+        return max(trav(root))
+        
