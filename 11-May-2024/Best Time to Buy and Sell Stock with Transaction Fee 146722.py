b1c3a970f352a7a82098b979669fd776c4aab3eb
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2024-05-11 07:48:56 +0300

    Created 'Best Time to Buy and Sell Stock with Transaction Fee' by A2SV Hub

---
+++ 11-May-2024/Best Time to Buy and Sell Stock with Transaction Fee 146722.py
@@ -0,0 +1,25 @@
+# Problem: Best Time to Buy and Sell Stock with Transaction Fee - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        new_prices = [(prices[i],i) for i in range(len(prices))]
+        graph = defaultdict(list)
+        mono_stack = []
+        for i in range(len(prices)-1,-1,-1):
+            while mono_stack and mono_stack[-1][0] < prices[i]:
+                mono_stack.pop()
+            graph[i].extend(mono_stack[:])
+            mono_stack.append((prices[i],i))
+        cache = {}
+        def profit(i):
+            if i >= len(prices)-1:
+                return 0
+            if i in cache:
+                return cache[i]
+            max_profit = profit(i+1)
+            for p,j in graph[i]:
+                trade = prices[j] - prices[i] - fee + profit(j+1)
+                max_profit = max(trade,max_profit)
+            cache[i] = max_profit
+            return max_profit
+        return profit(0)
