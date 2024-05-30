a80dfbf9c2a69c480fb3d68a170223e01045d8f9
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2024-05-30 17:38:20 +0300

    Update knapsack2_dp.py

--- dynamic programming/AtCoders/Knapsack_2/knapsack2_dp.py
+++ dynamic programming/AtCoders/Knapsack_2/knapsack2_dp.py
@@ -1,5 +1,7 @@
 # https://atcoder.jp/contests/dp/tasks
 
+# https://atcoder.jp/contests/dp/tasks
+
 
 n, weight = map(int, input().split())
 arr = []
@@ -12,17 +14,44 @@ for _ in range(n):
     arr.append([w, v])
 arr.sort()
 
-vals = [[float('inf'), set()]] * (max_val + 1)
-vals[0] = [0, set()]
-for val in range(1, max_val + 1):
-    min_w = vals[val]
-    for i, (w, v) in enumerate(arr):
-        if val - v >= 0 and i not in vals[val-v][1]:
-            min_w = min([w + vals[val-v][0], set([i]) | vals[val-v][1]], min_w)
-    vals[val] = min_w
+vals = [float('inf')] * (max_val + 1)
+vals[0] = 0
+for i, (w, v) in enumerate(arr):
+    for val in range(max_val, -1, -1):
+        min_w = vals[val]
+        if val - v >= 0:
+            min_w = min(w + vals[val-v], min_w)
+        vals[val] = min_w
 
 for val in range(1, max_val + 1):
-    if vals[val][0] <= weight:
+    if vals[val] <= weight:
         ans = val
 
 print(ans)
+
+
+# n, weight = map(int, input().split())
+# arr = []
+# max_val = 0
+# ans = 0
+# for _ in range(n):
+#     w, v = map(int, input().split())
+#     ans = max(v, ans)
+#     max_val += v
+#     arr.append([w, v])
+# arr.sort()
+
+# vals = [[float('inf'), set()]] * (max_val + 1)
+# vals[0] = [0, set()]
+# for val in range(1, max_val + 1):
+#     min_w = vals[val]
+#     for i, (w, v) in enumerate(arr):
+#         if val - v >= 0 and i not in vals[val-v][1]:
+#             min_w = min([w + vals[val-v][0], set([i]) | vals[val-v][1]], min_w)
+#     vals[val] = min_w
+
+# for val in range(1, max_val + 1):
+#     if vals[val][0] <= weight:
+#         ans = val
+
+# print(ans)
