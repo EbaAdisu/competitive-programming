522910da670ad995d069f50005a43454be79ac7d
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2024-06-20 06:48:33 +0300

    Created 'Maximal Square' by A2SV Hub

---
+++ 20-Jun-2024/Maximal Square 170155.py
@@ -0,0 +1,17 @@
+# Problem: Maximal Square - https://leetcode.com/problems/maximal-square/
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        def inbound(r,c):
+            if 0<=r<m and 0<=c<n:
+                return mat[r][c]
+            return 0
+        m, n = len(matrix), len(matrix[0])
+        ans = 0
+        mat = [[0]*n for i in range(m)]
+        for r in range(m):
+            for c in range(n):
+                if matrix[r][c] == '1':
+                    mat[r][c] = min(inbound(r-1, c-1) + 1, inbound(r-1, c) + 1, inbound(r,c-1) + 1)
+                ans = max(mat[r][c], ans)
+        return ans ** 2
