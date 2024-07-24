f65fa065a37a37981bb0160054ecc350a6b38497
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2024-07-24 09:44:33 +0300

    Created 'They Are Everywhere' by A2SV Hub

---
+++ 24-Jul-2024/They Are Everywhere 183492.py
@@ -0,0 +1,17 @@
+# Problem: They Are Everywhere - https://codeforces.com/problemset/problem/701/C
+
+from collections import Counter
+n = int(input())
+s = input()
+total = len(Counter(s))
+window = Counter()
+l = 0
+ans = n
+for r in range(n):
+    window[s[r]] += 1
+    while total == len(window) and window[s[l]] > 1:
+        window[s[l]] -= 1
+        l += 1
+    if total == len(window):
+        ans = min(ans, r - l + 1)
+print(ans)
