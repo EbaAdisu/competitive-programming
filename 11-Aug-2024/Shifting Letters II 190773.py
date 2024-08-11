97a75beeba9039c6bcdc7b22f67f5555ef0dba73
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2024-08-11 23:11:47 +0300

    Created 'Shifting Letters II' by A2SV Hub

---
+++ 11-Aug-2024/Shifting Letters II 190773.py
@@ -0,0 +1,18 @@
+# Problem: Shifting Letters II - https://leetcode.com/problems/shifting-letters-ii/
+
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        shift = [0]*(len(s)+1)
+        for l, r, v in shifts:
+            v = 1 if v == 1 else -1
+            shift[l] += v
+            shift[r+1] -= v
+        for i in range(1,len(shift)):
+            shift[i]+=shift[i-1]
+        # print(shift)
+        ans = ''
+        for i in range(len(shift)-1):
+            ans += letters[(letters.index(s[i])+shift[i])%26]
+            # print(letters.index(s[i]))
+        return ans
