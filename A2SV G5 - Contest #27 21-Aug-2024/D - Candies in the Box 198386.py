7d54df4a923865a840ab7631a5e106bc6d8e3f41
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2024-08-21 15:06:19 +0300

    Created 'D - Candies in the Box' by A2SV Hub

---
+++ A2SV G5 - Contest #27 21-Aug-2024/D - Candies in the Box 198386.py
@@ -0,0 +1,24 @@
+# Problem: D - Candies in the Box - https://codeforces.com/gym/538762/problem/D
+
+# https://codeforces.com/gym/538762
+def valid(temp, k):
+    v = 0
+    while temp >= 10 and temp >= k:
+        v += k
+        temp -= k
+        if temp >= 10:
+            temp -= temp//10
+    return 2*(temp + v) >= n
+
+
+n = int(input())
+
+l = 1
+r = n
+while l < r:
+    m = (l+r)//2
+    if valid(n, m):
+        r = m
+    else:
+        l = m + 1
+print(r)
