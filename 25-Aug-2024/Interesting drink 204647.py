8c50f4f28c7d4a67abe2eb73abad1b8bf8a63dcd
EbaAdisu <120240242+EbaAdisu@users.noreply.github.com>
2024-08-25 21:38:12 +0300

    Created 'Interesting drink' by A2SV Hub

---
+++ 25-Aug-2024/Interesting drink 204647.py
@@ -0,0 +1,10 @@
+# Problem: Interesting drink - https://codeforces.com/problemset/problem/706/B/
+
+from bisect import bisect_left, bisect_right
+n = int(input())
+nums = sorted(map(int, input().split()))
+
+t = int(input())
+for _ in range(t):
+    m = int(input())
+    print(bisect_right(nums, m))
